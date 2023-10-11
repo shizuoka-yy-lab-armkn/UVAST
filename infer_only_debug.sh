ca2bdb1e81a5bb28391d9f62388d34c6c481ad68
arumakan1727 <33191176+arumakan1727@users.noreply.github.com>
2023-10-11 11:15:18 +0900

    chore: add infer_only_debug.sh

---
+++ infer_only_debug.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+source ./_common.sh
+set -euo pipefail
+
+#------------------------------------------------
+# parse args
+if [[ $# -ne 5 ]]; then
+	echo "Usage: $0 {dataset_dir_path} {splitX} {inference_model_path} {f1_labelwise_csv_path} {framewise_inference_result_dir}"
+	exit 1
+fi
+
+dataset_dir_path="$1"
+split="$2"
+inference_model_path="$3"
+f1_labelwise_csv_path="$4"
+framewise_inference_result_dir="$5"
+
+data_root="$(dirname "$dataset_dir_path")"
+dataset_name="$(basename "$dataset_dir_path")"
+
+splitID="$(echo "$split" | sed -E 's/^split([0-9]+)$/\1/')"
+if [[ -z $splitID ]]; then
+	echo "Invalid name of split: $split"
+	exit 1
+fi
+
+python run.py \
+	${common_train_stage2_args} \
+	--dataset "$dataset_name" \
+	--data_root "$data_root" \
+	--split "$splitID" \
+	--inference_only \
+	--path_inference_model "$inference_model_path" \
+	--f1_labelwise_csv_path "$f1_labelwise_csv_path" \
+	--framewise_inference_result_dir "$framewise_inference_result_dir" \
+  --debug
